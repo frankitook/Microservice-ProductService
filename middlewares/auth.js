b406dceb9ec38651635c5c1440fe3827946c15e6
Franco <francogimenez981@gmail.com>
2024-10-23 14:34:27 -0300

    asd

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,20 +2,21 @@ require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 function verificarToken(req, res, next) {
-    const token = req.headers['authorization'];
-    if (!token) {
-      return res.status(403).send('Token requerido');
-    }
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) {
+     return res.status(403).send('Token requerido');
+  }
   
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
         return res.status(401).send('Token inválido');
       }
-       
+      req.cliente = decoded.cliente;
       next();
     });
   }
 
   module.exports = {verificarToken};
 
-  
+
