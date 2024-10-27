0175fcbdab873e3cac90af5dcc74fc713609355b
Franco Gimenez <89666504+frankitook@users.noreply.github.com>
2024-10-27 13:39:44 -0300

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -26,33 +26,54 @@ const obtenerProductoPorId = async (req, res) => {
 };
 
 
+
+
 const crearProducto = async (req, res) => {
-    const { imagenes, descripcion, precio, nroDni, idCategoria, codSub } = req.body;
+    const { descripcion, precio, idCategoria, codSub } = req.body;
+
+   
+    const imagen1 = req.files['imagen1'] ? req.files['imagen1'][0].buffer : null;
+    const imagen2 = req.files['imagen2'] ? req.files['imagen2'][0].buffer : null;
+    const imagen3 = req.files['imagen3'] ? req.files['imagen3'][0].buffer : null;
+
     try {
-        const nuevoProducto = await Producto.create({ imagenes, descripcion, precio, nroDni, idCategoria, codSub });
-        res.status(201).json(nuevoProducto);
+        const nuevoProductoData = {
+            descripcion,
+            precio,
+            idCategoria,
+            codSub,
+            imagen1,
+            imagen2,
+            imagen3,
+        };
+
+        const nuevoProducto = await Producto.create(nuevoProductoData);
+
+        res.status(201).json({ message: 'Producto creado correctamente', producto: nuevoProducto });
     } catch (error) {
-        res.status(500).json({ message: 'Error al crear el producto' });
+        res.status(500).json({ message: 'Error al crear el producto', error: error.message });
     }
 };
+  
+
 
 
 const actualizarProducto = async (req, res) => {
-    const { idProducto } = req.params; 
-    const { descripcion, precio, imagenes, idCategoria, codSub, nroDni } = req.body; 
+    const { idProducto } = req.params;
+    const { descripcion, precio, idCategoria, codSub } = req.body;
+
+    const imagen1 = req.files['imagen1'] ? req.files['imagen1'][0].buffer : null;
+    const imagen2 = req.files['imagen2'] ? req.files['imagen2'][0].buffer : null;
+    const imagen3 = req.files['imagen3'] ? req.files['imagen3'][0].buffer : null;
 
     try {
-        const producto = await Producto.findOne({ where: { idProducto } }); 
+        const producto = await Producto.findOne({ where: { idProducto } });
         if (producto) {
-            
             if (descripcion !== undefined) {
                 producto.descripcion = descripcion;
             }
             if (precio !== undefined) {
                 producto.precio = precio;
-            }
-            if (imagenes !== undefined) {
-                producto.imagenes = imagenes;
             }
             if (idCategoria !== undefined) {
                 producto.idCategoria = idCategoria;
@@ -60,19 +81,20 @@ const actualizarProducto = async (req, res) => {
             if (codSub !== undefined) {
                 producto.codSub = codSub;
             }
-            if (nroDni !== undefined) {
-                producto.nroDni = nroDni;
-            }
+            if (imagen1) producto.imagen1 = imagen1;
+            if (imagen2) producto.imagen2 = imagen2;
+            if (imagen3) producto.imagen3 = imagen3;
 
-            await producto.save(); 
-            res.json(producto); 
+            await producto.save();
+            res.json(producto);
         } else {
-            res.status(404).json({ message: 'Producto no encontrado' }); 
+            res.status(404).json({ message: 'Producto no encontrado' });
         }
     } catch (error) {
-        res.status(500).json({ message: 'Error al actualizar el producto' }); 
+        res.status(500).json({ message: 'Error al actualizar el producto' });
     }
 };
+
 
 
 const eliminarProducto = async (req, res) => {
