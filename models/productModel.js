f1a58ec1ebbdd4790afe9f632f351140dbf67fdb
Franco Gimenez <89666504+frankitook@users.noreply.github.com>
2024-10-27 13:40:22 -0300

    Update productModel.js

--- models/productModel.js
+++ models/productModel.js
@@ -3,13 +3,11 @@ const base = require('../configuration/db');
 
 const Producto = base.define('Producto', {
     idProducto: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    imagenes: { type: DataTypes.BLOB('long'), allowNull: true },
+    imagen1: { type: DataTypes.BLOB('long'), allowNull: true }, 
+    imagen2: { type: DataTypes.BLOB('long'), allowNull: true }, 
+    imagen3: { type: DataTypes.BLOB('long'), allowNull: true }, 
     descripcion: { type: DataTypes.STRING, allowNull: false },
     precio: { type: DataTypes.FLOAT, allowNull: false },
-    nroDni: {
-        type: DataTypes.INTEGER,  
-        allowNull: false 
-    },
     idCategoria: {
         type: DataTypes.INTEGER,
         references: {
